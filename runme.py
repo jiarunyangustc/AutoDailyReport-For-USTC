53fe963ed3bc58a5f8a6235a1f216d31938fb2b7
jiarunyangustc <86905633+jiarunyangustc@users.noreply.github.com>
2022-05-30 21:45:50 +0800

    Update runme.py

--- runme.py
+++ runme.py
@@ -3,8 +3,8 @@ from Report import Report
 
 report_data = [
     ("juzhudi", "西校区"),  # 居住地
-    ("dorm_building", "z2"),  # 宿舍楼
-    ("dorm", "414"),  # 宿舍号
+    ("dorm_building", "8"),  # 宿舍楼
+    ("dorm", "115"),  # 宿舍号
     ("body_condition", "1"),  # 身体状况 1为正常
     ("body_condition_detail", ""),  # 身体状况详细 正常无需
     ("now_status", "1"),  # 当前状态 1为正常在校园内
@@ -17,15 +17,15 @@ report_data = [
     ("is_goto_danger", "0"),  # 14天内是否有疫情中高风险地区旅居史 0为否
     ("jinji_lxr", "cyzkrau"),  # 紧急联系人
     ("jinji_guanxi", "self"),  # 与本人关系
-    ("jiji_mobile", "18701375065"),  # 紧急联系人电话
+    ("jiji_mobile", "17812117953"),  # 紧急联系人电话
     ("other_detail", ""),  # 其他情况说明
 ]
 cross_campus_data = [
     ("return_college[]", "东校区"),  # 往返校区
-    ("return_college[]", "西校区"),  # 往返校区
-    ("return_college[]", "南校区"),  # 往返校区
-    ("return_college[]", "北校区"),  # 往返校区
-    ("return_college[]", "中校区"),  # 往返校区
+#     ("return_college[]", "西校区"),  # 往返校区
+#     ("return_college[]", "南校区"),  # 往返校区
+#     ("return_college[]", "北校区"),  # 往返校区
+#     ("return_college[]", "中校区"),  # 往返校区
     ("reason", "上课"),  # 原因
 ]
 out_school_data = [
@@ -46,8 +46,7 @@ if __name__ == "__main__":
     while count != 0:
         if (autorepoter.report(report_data)
                 & autorepoter.upload_code()
-                & autorepoter.cross_campus(cross_campus_data)
-                & autorepoter.out_school(out_school_data)):
+                & autorepoter.cross_campus(cross_campus_data):
             print("ENJOY YOUR FREEDOM! ")
             break
         print("Retry...")
